b8124751099019351910b727f7949ed284d31b1e
Bartasa Mwangangi <trabbart8@gmail.com>
2025-03-24 14:22:52 +0300

    npm update

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "jsm-x9",
-    project: "javascript-react"
-  })],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "jsm-x9",
+      project: "javascript-react"
+    })
+  ],
 
   build: {
     sourcemap: true
-  }
-})
+  },
+
+  // Ensure compatibility with GitHub Pages deployment
+  base: '/apple_website/', // Change 'repo-name' to your actual GitHub repo name
+});
